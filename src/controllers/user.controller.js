6c2d4afb8dc5b588874d601af3cdb6e107553638
Gerardo Rodriguez <gdrodriguezok@gmail.com>
2023-06-01 21:23:39 -0300

    user profile populated

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -39,8 +39,14 @@ export const createUser = async (req, res) => {
 }
 
 export const getMyProfile = async (req, res) => {
-  const user = await User.findById(req.userId, { password: 0, active: 0, createdAt: 0, updatedAt: 0 }).populate("roles")
-  console.log(user.roles[0].name);
+  const user = await User.findById(req.userId,
+    { password: 0, active: 0, createdAt: 0, updatedAt: 0 })
+    .populate("roles")
+    .populate({
+      path: 'organization',
+      select: 'name'
+    })
+  // console.log(user.roles[0].name);
   if (!user) return res.status(404).json({ message: "No user found" });
   return res.status(200).json(user);
 }
@@ -60,20 +66,20 @@ export const updateUser = async (req, res) => {
   const { email, first_name, last_name, organization, country, roles } = req.body;
   const admin = req.user.roles.some(role => role.name === "admin")
   const user = await User.findById(userId)
-  if(!user) return res.status(404).json({ message: "User not found" })
-  if(req.user) {
-    if(!admin) return res.status(401).json({ message: "Unauthorized" })
+  if (!user) return res.status(404).json({ message: "User not found" })
+  if (req.user) {
+    if (!admin) return res.status(401).json({ message: "Unauthorized" })
   }
   try {
-    if(email) user.email = email
-    if(first_name) user.first_name = first_name
-    if(last_name) user.last_name = last_name
-    if(organization) user.organization = organization
-    if(country) user.country = country
-    if(roles) {
+    if (email) user.email = email
+    if (first_name) user.first_name = first_name
+    if (last_name) user.last_name = last_name
+    if (organization) user.organization = organization
+    if (country) user.country = country
+    if (roles) {
       const rolesFound = await Role.find({ name: { $in: roles } })
       user.roles = rolesFound.map(role => role._id)
-      if(!rolesFound) return res.status(404).json({ message: "Roles not found" })
+      if (!rolesFound) return res.status(404).json({ message: "Roles not found" })
     }
     await user.save()
     return res.status(200).json({ message: "User updated successfully" })
