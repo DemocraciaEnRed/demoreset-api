a5b1085af0cd099cee5220e5b59b81f0101916c7
Guillermo Croppi <guillermocroppi@gmail.com>
2023-07-26 12:17:35 -0300

    Testing secure settings in nodemailer

--- src/libs/nodemailer.js
+++ src/libs/nodemailer.js
@@ -5,7 +5,7 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
   host: process.env.NODEMAILER_HOST,
   port: process.env.NODEMAILER_PORT,
-  secure: process.env.NODEMAILER_SECURE,
+  secure: true,
   auth: {
     user: process.env.NODEMAILER_EMAIL,
     pass: process.env.NODEMAILER_PASSWORD
